cfa853b7749e9119e0fe7cf0d17ecd58fc514203
Darlan Thiago <darlanthiago53@gmail.com>
2024-12-26 12:45:02 -0300

    feat: no auth required on horizon

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -29,9 +29,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     protected function gate(): void
     {
         Gate::define('viewHorizon', function (User $user = null) {
-            return in_array($user->email, [
-                //
-            ]);
+            return true;
         });
     }
 }
